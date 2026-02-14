febce570523d25b554de89cd48bf99fde68727ca
vishalbhatoy <90851307+vishalbhatoy@users.noreply.github.com>
2026-02-14 15:54:24 -0500

    Add files via upload

---
+++ Restriction of S4 to C4.m
@@ -0,0 +1,26 @@
+G:=SymmetricGroup(4);
+H:=sub< G | (1,2,3,4)>;
+bG:=Basis(CharacterRing(G));
+
+trv:=bG[1];
+det:=bG[2];
+ano:=bG[3];
+stntensordet:=bG[4];
+stn:=bG[5];
+
+
+
+Restriction(trv,H);
+Restriction(det,H);
+Restriction(ano,H);
+Restriction(stntensordet,H);
+Restriction(stn,H);
+
+[Restriction(v,H): v in bG];
+
+bH:=Basis(CharacterRing(H));
+bH;
+
+[[InnerProduct(Restriction(v,H),x):x in bH] : v in bG];
+
+
